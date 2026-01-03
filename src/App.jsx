fa9c41d9d7b4d84941c9c506f128604727ec34a6
Mydvthukran <my3596418@gmail.com>
2026-01-03 21:28:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WeatherCard from './Components/WeatherCard'
+
+const App = () => {
+  return (
+    <div>
+      <WeatherCard></WeatherCard>
+    </div>
+  )
+}
+
+export default App
